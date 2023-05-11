189ad5eadfdae1cb0be91f5ffbaa5f570bed46c5
Elia Partin <arenaselia@gmail.com>
2023-05-11 07:59:15 -0400

    added disabled cards function

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
 (function () {
   "use strict";
 
-  const flippedCards = [];
-  const matchedCards = [];
+  let flippedCards = []; //the 2 cards that are flipped
+  let matchedCards = []; //array to keep track of number of matched cards
   const htmlContainer = document.querySelector(".container");
 
   const cards = [
@@ -32,19 +32,18 @@
       url: "https://octodex.github.com/images/boxertocat_octodex.jpg",
     },
   ];
-  const deck = [...cards, ...cards];
+  const deck = [...cards, ...cards]; //putting the cards into the deck
+  const numOfTotalCardsInGame = deck.length; //total cards in the game
   const cardsContainer = document.querySelector(".container");
-  let flipCards = [],
-    matchCards = [];
 
-  const playButton = document.querySelector("#play-button");
+  const playButton = document.querySelector("#play-button"); //button to start the game not being used at the moment
 
-  shuffleCards(deck);
+  shuffleCards(deck); //all the cards get shuffled
   function shuffleCards(arr) {
     return arr.sort(function () {
       return Math.random() - 0.5;
     });
-  }
+  };
 
   function generateCardsHTML(deck, container) {
     let html = "";
@@ -64,34 +63,52 @@
     }
     container.innerHTML = "";
     container.insertAdjacentHTML("afterbegin", html);
-  }
+  };
 
-  function unflippedCards() {
+  
+  
+  play();// the game is started
+
+  function checkForMatch() {
+    let matched = flippedCards[0].dataset.name === flippedCards[1].dataset.name;
+    
+    if (matched) {
+      console.log("Cards Match!")
+      matchedCards.push(flippedCards[0]);
+      matchedCards.push(flippedCards[1]);
+      disableCards(flippedCards[0],flippedCards[1]);
+      checkForMatch(".open").removeClass("myCardsArray").addClass("matchedCards");
+    } else {
+      console.log("No Match :( ");
+      unFlipCards();
+      checkForMatch(".open").removeClass("matchedCards").addClass("myCardsArray");
+    }
+  };
+  
+  function unFlipCards(){
     flippedCards.forEach(function (card) {
       setTimeout(function () {
         card.classList.remove("flip");
       }, 1000);
     });
     flippedCards = [];
-  }
-  //compare flipped cards to see if they match
-  //[node1, node2]
-  function checkForMatch(flippedCards) {
-    let match = flippedCards[0].dataset.deck === flippedCards[1].dataset.deck;
-    let matchCard = matchedCards[0];
-    if (match) {
-      checkForMatch(".open").removeClass("myCardsArray").addClass("matchedCards");
-    } else {
-      checkForMatch(".open").removeClass("matchedCards").addClass("myCardsArray");
+  };
 
-      //TODO do this first if the cards do not match we need to flip the cards back over
+  function disableCards(card1, card2){
+    card1.removeEventListener("click", flipCard);
+    card2.removeEventListener("click", flipCard); 
 
-      // setTimeout(function () {
-      //     deck('.open').addClass('matchedCards').removeClass('myCardsArray');
-      // }, 500);
-      //unflipCards();
+    flippedCards.forEach(function (card) {
+      setTimeout(function () {
+        card.classList.remove("unFlipCards");
+      }, 1000);
+    });
+    flippedCards = []; 
+    if(matchedCards.length === numOfTotalCardsInGame){
+      alert("You won!");
     }
-  }
+
+  };
 
   function removeCards(card1, card2, cards) {
     // flippedCards.forEach(function ())
@@ -102,45 +119,23 @@
       if (card1 !== cards[i] || card2 !== cards[i]) {
         // comparing card1 and 2 to the element in the array which is technically a card
         // resizedArray.push(originalArray[i]);
-        card1.removeEvenListener("click", flipCard);
-        card2.removeEvenListener("click", flipCard);
+        card1.removeEventListener("click", flipCard);
+        card2.removeEventListener("click", flipCard);
 
         // only keeping the cards that are not a match
       }
     }
     return resizedArray;
-  }
-  /* TODO: if not needed delete all this 
-//check to see if two cards match
-  function checkForMatch(card1, card2, myCardsArray, matchedCards) {
-//     // you need to compare the two value inside the flipped cards array to see if they match
-//     let resizedArray = [];
-    if (card1 === card2) {
-        myCardsArray('.open').removeClass('myCardsArray').addClass('matchedCards');
-    //   resizedArray = removeCards(card1, card2, myCardsArray);
-    //   matchedCards.push(card1);
-    //   matchedCards.push(card2);
-    //   return resizedArray;
-    } else {
-    //   return myCardsArray;
-    setTimeout(function () {
-        myCardsArray('.open').addClass('matchedCards').removeClass('myCardsArray');
-    }, 500);
-    };
-    // if ($('.match').length === 12) {
-//        winGame();
-//        alert("You won!");
-//     };
-  }
-*/
-  function flipCard() {
+  };
+
+  function flipCard() { //flipCard is the event inside play function
     // alert('firing')
     this.classList.add("flip");
     flippedCards.push(this);
     if (flippedCards.length === 2) {
-      checkForMatch(flippedCards);
+      checkForMatch();
     }
-  }
+  };
 
   function play() {
     const deck = [...cards, ...cards];
@@ -151,7 +146,5 @@
     for (let i = 0; i < cardNodes.length; i++) {
       cardNodes[i].addEventListener("click", flipCard);
     }
-  }
-
-  play();
+  };
 })();
